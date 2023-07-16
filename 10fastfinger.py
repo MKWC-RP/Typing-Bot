de207caeda8ceb87d1e104cf66d8378203e052fe
MKWC-RP <66546226+MKWC-RP@users.noreply.github.com>
2023-07-16 11:53:58 +0530

    Add files via upload

---
+++ 10fastfinger.py
@@ -0,0 +1,46 @@
+from selenium import webdriver
+import time
+
+path = "D:\Python\chromedriver.exe"
+driver = webdriver.Chrome(path)
+
+driver.get("https://10fastfingers.com/advanced-typing-test/english")
+driver.maximize_window()
+time.sleep(10)
+while True:
+    count = 0
+    while True:
+        try:
+            first_word = driver.find_element_by_class_name('highlight')
+        except Exception:
+            timer = driver.find_element_by_id('timer')
+            t1 = timer.text.split(':')
+            t = int(t1[1]) 
+            t_time = 60 - t
+            print(f'You wrote {count} words in {t_time} seconds!!')
+            print(f'you average wpm was {(count * 60) / t_time}\n')
+            while True:
+                reload = driver.find_element_by_id('reload-btn')
+                reload.click()
+                time.sleep(5)
+                break
+            break
+        else:
+            words = driver.find_element_by_xpath('//*[@id="words"]')
+            first = first_word.text
+            a = words.text
+            index = a.index(first)
+            a = a[index:len(a)]
+            a1 = a.split()
+            count += len(a1)
+            timer = driver.find_element_by_id('timer')
+            if timer.text == '0:00':
+                print('The race has ended!!!')
+                exit()
+            else:
+                input_field = driver.find_element_by_xpath('//*[@id="inputfield"]')
+                #time.sleep(1)
+                print(f'{a}, ')
+                input_field.send_keys(a + ' ')
+                #time.sleep(1)
+    time.sleep(2)
